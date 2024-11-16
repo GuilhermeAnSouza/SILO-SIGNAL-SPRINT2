697d21de2ce0d8ee5314483012e45417c2a6ee21
mrclhnrq <marcelo.soares@sptech.school>
2024-11-16 00:02:38 -0300

    Validação Cadastro - Usuario

--- web-data-viz-silotech/src/controllers/usuarioController.js
+++ web-data-viz-silotech/src/controllers/usuarioController.js
@@ -1,5 +1,5 @@
 var usuarioModel = require("../models/usuarioModel");
-var aquarioModel = require("../models/aquarioModel");
+// var aquarioModel = require("../models/aquarioModel");
 
 function autenticar(req, res) {
     var email = req.body.emailServer;
@@ -59,26 +59,26 @@ function autenticar(req, res) {
 function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
     var nome = req.body.nomeServer;
-    var email = req.body.emailServer;
+    var telUsuario = req.body.telUsuarioServer;
+    var emailUsuario = req.body.emailUsuarioServer;
     var senha = req.body.senhaServer;
     var fkEmpresa = req.body.idEmpresaVincularServer;
-    var cpf = req.body.cpfServer;
 
     // Faça as validações dos valores
     if (nome == undefined) {
         res.status(400).send("Seu nome está undefined!");
-    } else if (email == undefined) {
+    } else if (emailUsuario == undefined) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
     } else if (fkEmpresa == undefined) {
         res.status(400).send("Sua empresa a vincular está undefined!");
-    } else if (cpf == undefined) {
-        res.status(400).send("Seu CPF está undefined!");
+    } else if (telUsuario == undefined) {
+        res.status(400).send("Seu telefone está undefined!");
     } else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha, fkEmpresa, cpf)
+        usuarioModel.cadastrar(nome, telUsuario, emailUsuario, senha, fkEmpresa)
             .then(
                 function (resultado) {
                     res.json(resultado);
