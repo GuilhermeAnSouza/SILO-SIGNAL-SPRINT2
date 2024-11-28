cdc29a13b1395a3d9667cd01f49da6a3c289f70e
ShirleyFerr <shirley.lima@sptech.school>
2024-11-28 16:38:30 -0300

    rota da dash e funcoes

--- web-data-viz-silotech/src/controllers/dashboardController.js
+++ web-data-viz-silotech/src/controllers/dashboardController.js
@@ -1,21 +1,21 @@
 var dashboardModel = require("../models/dashboardModel")
 
-// function buscarSiloSensoresEmpresa(){
+function buscarSiloSensoresEmpresa(){
  
-// var idEmpresa = req.params.idEmpresa
+var idEmpresa = req.params.idEmpresa
 
-//  dashboardModel.buscarSiloSensoresEmpresa(idEmpresa).then((resultado) => {
-//     if (resultado.length > 0) {
-//       res.status(200).json(resultado);
-//     } else {
-//       res.status(204).json([]);
-//     }
-//   }).catch(function (erro) {
-//     console.log(erro);
-//     console.log("Houve um erro ao buscar os aquarios: ", erro.sqlMessage);
-//     res.status(500).json(erro.sqlMessage);
-//   });
-// }
+ dashboardModel.buscarSiloSensoresEmpresa(idEmpresa).then((resultado) => {
+    if (resultado.length > 0) {
+      res.status(200).json(resultado);
+    } else {
+      res.status(204).json([]);
+    }
+  }).catch(function (erro) {
+    console.log(erro);
+    console.log("Houve um erro ao buscar os aquarios: ", erro.sqlMessage);
+    res.status(500).json(erro.sqlMessage);
+  });
+}
 
 function kpis_silo1(req, res) {
   var idEmpresa = req.params.idEmpresa
@@ -54,7 +54,7 @@ function kpis_silo2(req, res) {
 }
 
 module.exports = {
-    // buscarSiloSensoresEmpresa,
+    buscarSiloSensoresEmpresa,
     kpis_silo1,
     kpis_silo2
 }
--- web-data-viz-silotech/src/models/dashboardModel.js
+++ web-data-viz-silotech/src/models/dashboardModel.js
@@ -1,15 +1,15 @@
 var database = require("../database/config");
 
 
-// function buscarSiloSensoresEmpresa(empresaId) {
+function buscarSiloSensoresEmpresa(empresaId) {
 
-//   var instrucaoSql = `
-//     SELECT * FROM silo JOIN sensor ON fk_silo = idSilo where fk_empresa = ${empresaId}
-//     `;
+  var instrucaoSql = `
+    SELECT * FROM silo JOIN sensor ON fk_silo = idSilo where fk_empresa = ${empresaId}
+    `;
 
-//   console.log("Executando a instrução SQL: \n" + instrucaoSql);
-//   return database.executar(instrucaoSql);
-// }
+  console.log("Executando a instrução SQL: \n" + instrucaoSql);
+  return database.executar(instrucaoSql);
+}
 
 function kpis_silo1(idEmpresa, idSilo) {
 
@@ -33,7 +33,7 @@ function kpis_silo2(idEmpresa, idSilo) {
 
 
 module.exports = {
-    // buscarSiloSensoresEmpresa,
+    buscarSiloSensoresEmpresa,
     kpis_silo1,
     kpis_silo2
 }
