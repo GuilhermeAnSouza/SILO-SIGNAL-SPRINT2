b523bf91b54c7c082a269905bc2557a44121dad5
joaoschettini <joao.ssantos@sptech.school>
2024-12-03 01:23:13 -0300

    css dos alertas / acerto dos hrefs nas de cadastro / função pra alerta dos dois silos

--- web-data-viz-silotech/src/database/SCRIPT-PROJETO.sql
+++ web-data-viz-silotech/src/database/SCRIPT-PROJETO.sql
@@ -15,6 +15,9 @@ CREATE TABLE empresa (
 insert into empresa (razaoSocial, cnpj, telEmp, emailEmp) values
 ('Sojas 1000 grau', '12345678914527', '11236589741', 'soja1000grau@gmail.com');
 
+insert into empresa (razaoSocial, cnpj, telEmp, emailEmp) values
+('Silo Tech', '98765434578961', '11980775433', 'silo@tech.com');
+
 select * from empresa;
 
 update empresa set fk_responsavel = 1 where idEmpresa = 1;
@@ -37,7 +40,22 @@ insert into usuario (nome, telComercial, email, senha) values
 insert into usuario (nome, telComercial, email, senha) values
 ('Guilherme', '11948794499', 'guilherme@hotmail.com', 'Gui@2020');
 
+insert into usuario (nome, telComercial, email, senha) values
+('João Roberto', '11987659998', 'joao@silotech.com', 'joao@2024'),
+('Marcelo Henrique', '11909871234', 'marcelo@silotech.com', 'marcelo@2024'),
+('Felippe Santos', '11929384567', 'felippe@silotech.com', 'felippe@2024'),
+('Shirley Ferreira', '11989412398', 'shirley@silotech.com', 'shirley@2024'),
+('Linya Mendonça', '11999098878', 'linya@silotech.com', 'linya@2024'),
+('Guilherme Antônio', '11956564747', 'guilherme@silotech.com', 'guilherme@2024');
+
 update usuario set fk_empresa = 1 where idUsuario = 2;
+
+update usuario set fk_empresa = 2 where idUsuario = 3;
+update usuario set fk_empresa = 2 where idUsuario = 4;
+update usuario set fk_empresa = 2 where idUsuario = 5;
+update usuario set fk_empresa = 2 where idUsuario = 6;
+update usuario set fk_empresa = 2 where idUsuario = 7;
+update usuario set fk_empresa = 2 where idUsuario = 8;
 
 select * from usuario;
 
@@ -80,8 +98,6 @@ insert into sensor(dtImplementacao, porcentagemDetec, dataHora, statusFuncioname
 
 SELECT sensor.porcentagemDetec FROM silo JOIN sensor ON fk_silo = idSilo where fk_empresa = 1 and fk_silo = 2;
 
-select * from sensor;
 
-select max(porcentagemDetec) as MaiorPercentMes from sensor where dataHora < '2024-11-28 14:12:00' AND dataHora > '2024-10-31 00:00:00';
 
 -- drop database silosignal;
